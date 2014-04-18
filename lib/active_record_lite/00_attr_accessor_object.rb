b46b4d5d6e33a32f04ab0bde9870e4096f6ceb12
App Academy Student <student@appacademy.io>
2014-04-18 14:01:40 -0700

    specs 00 and 02 passing

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,10 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |method_name|
+      define_method(method_name) { instance_eval("@#{method_name}") }
+      define_method("#{method_name}=") do |arg|
+        instance_eval("@#{method_name} = arg")
+       end
+    end
   end
 end
--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -4,52 +4,129 @@ require 'active_support/inflector'
 
 class MassObject
   def self.parse_all(results)
-    # ...
+    results.map { |data| new(data) }
   end
 end
 
 class SQLObject < MassObject
   def self.columns
-    # ...
+    @columns ||=
+    DBConnection.execute2("SELECT * FROM #{table_name}").first.map(&:to_sym)
+
+    if !@col_methods_initialized
+      @col_methods_initialized = true
+      define_column_methods
+    end
+
+    @columns
   end
 
-  def self.table_name=(table_name)
-    # ...
+  def self.table_name=(table_name = nil)
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.pluralize.underscore
+    @table_name
   end
 
   def self.all
-    # ...
+    parse_all(DBConnection.execute("
+    SELECT
+      #{table_name}.*
+    FROM
+      #{table_name}
+    "))
   end
 
   def self.find(id)
-    # ...
+    self.new(DBConnection.execute("
+    SELECT
+    #{table_name}.*
+    FROM
+    #{table_name}
+    WHERE
+    id = #{id}
+    LIMIT 1
+    ").first
+    )
+  end
+
+  def columns
+    self.class.columns
+  end
+
+  def table_name
+    self.class.table_name
+  end
+
+
+  # puts "hello"
+
+  def self.define_column_methods
+    p self
+    columns.each do |col|
+      self.send(:define_method, (col)) { @attributes[col] }
+      self.send(:define_method, ("#{col}=")) { |value| @attributes[col] = value }
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= Hash.new
   end
 
   def insert
-    # ...
+    cols = columns.reject{ |el| el == :id }
+    col_str = "( #{cols.join(', ')} )"
+    question_str = col_str.gsub(/\w+/, '?')
+
+    DBConnection.execute("
+    INSERT INTO
+    #{table_name} #{col_str}
+    VALUES
+    #{question_str}
+    ", *non_id_values)
+
+    attributes[:id] = DBConnection.last_insert_row_id
   end
 
-  def initialize
-    # ...
+  def initialize(params = {})
+    attrs = Hash.new
+    columns.each {|col| attrs[col] = nil}
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      fail "unknown attribute #{attr_name}" unless self.class.columns.include?(attr_name)
+      attrs[attr_name] = value
+    end
+    @attributes = attrs
+     self
   end
 
   def save
-    # ...
+    unless attributes[:id].nil?
+      update
+    else
+      insert
+    end
   end
 
   def update
-    # ...
+    set_str = attributes.map { |key, val| "#{key} = ?"}.join(', ')
+
+    DBConnection.execute("
+    UPDATE
+    #{table_name}
+    SET
+    #{set_str}
+    WHERE
+    id = #{attributes[:id]}
+    ", *attribute_values)
   end
 
   def attribute_values
-    # ...
+    attributes.values
+  end
+  def non_id_values
+    attributes.reject{|k,v| k == :id}.map(&:last)
   end
 end
